a6cccd73fae9c0ef769d8c5649c1409f17a5c933
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-21 10:14:27 +0800

    Add project files.

---
+++ Dogs/Breeds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dogs
+{
+    enum Breeds
+    {
+        Collie = 3,
+        Corgi = -9,
+        Dachshund = 7,
+        Pug = 0,
+        kerry = 5
+    }
+}
---
+++ Dogs/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dogs
+{
+    class Dog : IComparable<Dog>
+    {
+        public Breeds Breed { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(Dog other)
+        {
+            if (this.Breed > other.Breed) return -1;
+            if (this.Breed < other.Breed) return 1;
+            return -Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return $"A {this.Breed} named {this.Name}";
+        }
+    }
+}
